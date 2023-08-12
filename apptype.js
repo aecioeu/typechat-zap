042913c1db8e67c3d152604b2aa885a085d265fe
aeroimports <aeroimports2016@gmail.com>
2023-08-12 17:02:55 -0300

    update

--- apptype.js
+++ apptype.js
@@ -137,9 +137,9 @@ function disToTop() {
       difference = windowHeight - resizeHeight;
 
      //coloca o elemto em posicao
-      $(elInput).css("top", difference);
+      //$(elInput).css("top", difference);
       // diminui o tamanho da janela para evitar o bug em iphone
-      document.querySelector("typebot-standard").shadowRoot.querySelector("div").style.height = windowHeight - userBar
+      //document.querySelector("typebot-standard").shadowRoot.querySelector("div").style.height = windowHeight - userBar
 
       var lastMessages = document.querySelector("typebot-standard").shadowRoot.querySelectorAll("div > div > div.flex.flex-col.w-full.min-w-0.gap-2")
       idMessage = lastMessages.length - 1
